aec8dcfc81164f7bd863345025a6c629a8d0c95c
Noah Cunningham <45009673+cunningham-noah@users.noreply.github.com>
2018-11-13 11:58:45 -0600

    Update leaguestatscalculator.py

--- leaguestatscalculator.py
+++ leaguestatscalculator.py
@@ -1,1 +1,17 @@
-
+"""Attributes: HP, HP_Plus, HP5, HP5_Plus, MP, MP_Plus, MP5, MP5_Plus, Armor, Magic_Resist, Movement_Speed, Life_Steal, Lethality, 
+   Armor_Penetration, Attack_Damage, Ability_Power, Cooldown_Reduction"""
+   
+champions = {
+	"Aatrox":{"HP":0, "HP_Plus":0, "HP5":0, "HP5_Plus":0, "MP":0, "MP_Plus":0, "MP5":0, "MP5_Plus":0, "Armor":0,},
+	"Ahri":{"HP":0, "HP_Plus":0, "HP5":0, "HP5_Plus":0, "MP":0, "MP_Plus":0, "MP5":0, "MP5_Plus":0, "Armor":0,},
+	"Akali":{"HP":0, "HP_Plus":0, "HP5":0, "HP5_Plus":0, "MP":0, "MP_Plus":0, "MP5":0, "MP5_Plus":0, "Armor":0,},
+	"Alistar":{"HP":0, "HP_Plus":0, "HP5":0, "HP5_Plus":0, "MP":0, "MP_Plus":0, "MP5":0, "MP5_Plus":0, "Armor":0,},
+	"Amumu":{"HP":0, "HP_Plus":0, "HP5":0, "HP5_Plus":0, "MP":0, "MP_Plus":0, "MP5":0, "MP5_Plus":0, "Armor":0,},
+	"Anivia":{"HP":0, "HP_Plus":0, "HP5":0, "HP5_Plus":0, "MP":0, "MP_Plus":0, "MP5":0, "MP5_Plus":0, "Armor":0,},
+	"Annie":{"HP":0, "HP_Plus":0, "HP5":0, "HP5_Plus":0, "MP":0, "MP_Plus":0, "MP5":0, "MP5_Plus":0, "Armor":0,},
+	"Ashe":{"HP":0, "HP_Plus":0, "HP5":0, "HP5_Plus":0, "MP":0, "MP_Plus":0, "MP5":0, "MP5_Plus":0, "Armor":0,},
+	"Aurelion Sol":{"HP":0, "HP_Plus":0, "HP5":0, "HP5_Plus":0, "MP":0, "MP_Plus":0, "MP5":0, "MP5_Plus":0, "Armor":0,},
+	"Azir":{"HP":0, "HP_Plus":0, "HP5":0, "HP5_Plus":0, "MP":0, "MP_Plus":0, "MP5":0, "MP5_Plus":0, "Armor":0,},
+	"Bard":{"HP":0, "HP_Plus":0, "HP5":0, "HP5_Plus":0, "MP":0, "MP_Plus":0, "MP5":0, "MP5_Plus":0, "Armor":0,},
+	"Blitzcrank":{"HP":0, "HP_Plus":0, "HP5":0, "HP5_Plus":0, "MP":0, "MP_Plus":0, "MP5":0, "MP5_Plus":0, "Armor":0,},
+}
